554bbe09b8e30907dba418c9efee0d75f0e0149c
Camilo Higuita <chiguitar@unal.edu.co>
2020-09-24 10:23:05 -0500

    add kaboutdata

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,9 @@
 
 #include "src/utils/cinema.h"
 
+#include <KI18n/KLocalizedContext>
+#include <KI18n/KLocalizedString>
+
 #define CINEMA_URI "org.maui.cinema"
 
 static const  QList<QUrl>  getFolderVideos(const QString &path)
@@ -66,7 +69,10 @@ static const QList<QUrl> openFiles(const QStringList &files)
 
 Q_DECL_EXPORT int main(int argc, char *argv[])
 {
-	QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QCoreApplication::setAttribute(Qt::AA_DontCreateNativeWidgetSiblings);
+    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps, true);
+    QCoreApplication::setAttribute(Qt::AA_DisableSessionManager, true);
 
 #ifdef Q_OS_ANDROID
 	QGuiApplication app(argc, argv);
@@ -76,25 +82,29 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
 	QApplication app(argc, argv);
 #endif
 
-	app.setApplicationName("cinema");
-	app.setApplicationVersion(CINEMA_VERSION_STRING);
-	app.setApplicationDisplayName("Cinema");
 	app.setOrganizationName("Maui");
-	app.setOrganizationDomain(CINEMA_URI);
 	app.setWindowIcon(QIcon(":/img/assets/cinema.svg"));
-	MauiApp::instance()->setDescription("Video collection manager and player.");
-	MauiApp::instance()->setIconName("qrc:/img/assets/cinema.svg");
-	MauiApp::instance()->setWebPage("https://mauikit.org");
-	MauiApp::instance()->setDonationPage("https://mauikit.org");
-	MauiApp::instance()->setReportPage("https://invent.kde.org/camiloh/cinema");
-	MauiApp::instance()->setCredits ({QVariantMap({{"name", "Camilo Higuita"}, {"email", "milo.h@aol.com"}, {"year", "2019-2020"}})});
-	MauiApp::instance()->setHandleAccounts(false);
 
-	QCommandLineParser parser;
-	parser.setApplicationDescription("Video manager and player");
-	const QCommandLineOption versionOption = parser.addVersionOption();
-	parser.addOption(versionOption);
-	parser.process(app);
+	MauiApp::instance()->setIconName("qrc:/img/assets/cinema.svg");
+    MauiApp::instance()->setHandleAccounts(false);
+
+    KLocalizedString::setApplicationDomain("cinema");
+    KAboutData about(QStringLiteral("cinema"), i18n("Cinema"), CINEMA_VERSION_STRING, i18n("Video collection manager and player."),
+                     KAboutLicense::LGPL_V3, i18n("© 2020 Nitrux Development Team"));
+    about.addAuthor(i18n("Camilo Higuita"), i18n("Developer"), QStringLiteral("milo.h@aol.com"));
+    about.setHomepage("https://mauikit.org");
+    about.setProductName("maui/cinema");
+    about.setBugAddress("https://invent.kde.org/maui/buho/-/issues");
+    about.setOrganizationDomain(CINEMA_URI);
+    about.setProgramLogo(app.windowIcon());
+
+    KAboutData::setApplicationData(about);
+
+    QCommandLineParser parser;
+    parser.process(app);
+
+    about.setupCommandLine(&parser);
+    about.processCommandLine(&parser);
 
 	const QStringList args = parser.positionalArguments();
 
